0c01360254b971fbc2004c0eff6406bfd60c0b6f
test1234567894566 <51078585+test1234567894566@users.noreply.github.com>
2019-06-01 07:50:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`%bc Hyper Craft`,"http://twitch.tv/Death Shop")
+client.user.setGame(`%bc Royal server`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
